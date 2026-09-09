a8a9c38a8bcbe8b6c9be0b36d18edac98e8526aa
adelekeoluwapelumi17-coder <adelekeoluwapelumi17@gmail.com>
2026-09-09 13:39:09 +0100

    Added service detection to port scanner

--- port_scanner.py
+++ port_scanner.py
@@ -3,10 +3,15 @@ import socket
 target= input("Enter an IP address")
 print("scanning")
 
-for port in range(1, 11)
+for port in range(1, 1025):
   sock = socket.socket()
   sock.settimeout(0.5)
   result = sock.connect_ex((target, port))
   if result == 0:
     print("port", port, "is OPEN")
+    try:
+      service = socket.getservbyport(port)
+    except OSError:
+      service = "Unknown"
+    print("Service:", service)
   sock.close()
